585e468ec3eba56d65ec1ef74cef3568db17f009
Brenda Carrera <brenda.huitzil.26@gmail.com>
2023-06-14 12:55:28 -0600

    Agregar archivos de proyecto.

---
+++ WebServicesApiRest/Controllers/DatosController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebServicesApiRest.Models;
+
+namespace WebServicesApiRest.Controllers
+{
+    public class DatosController : ApiController
+    {
+        DatabaseRegistrosEntities conn = new DatabaseRegistrosEntities();
+
+        [HttpGet]
+        public List<DatosEntities> listaDatos ()
+        {
+            List<DatosEntities> datos = new List<DatosEntities>();
+            var datosBD = conn.DatosRegistro.ToList();
+            foreach (var item in datosBD)
+            {
+                datos.Add(new DatosEntities
+                {
+                    
+                    Compania = item.Compania,
+                    Contacto = item.Contacto,
+                    Correo = item.Correo,
+                    Telefono = item.Telefono
+                    
+                });
+            }
+            return datos;
+            
+        }
+        [HttpPost]
+        public bool datos(DatosEntities newDato)
+        {
+            bool response = false;
+            try
+            {
+                DatosRegistro addDatos = (new DatosRegistro { Compania = newDato.Compania,
+                    Contacto = newDato.Contacto,
+                    Correo = newDato.Correo,
+                    Telefono = newDato.Telefono
+                });
+
+                conn.DatosRegistro.Add(addDatos);
+                conn.SaveChanges();
+                response = true;
+
+            }
+            catch (Exception )
+            {
+                throw;
+            }
+            return response;
+        }
+
+
+    }
+}
---
+++ WebServicesApiRest/Models/DatosEntities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServicesApiRest.Models
+{
+    public class DatosEntities
+    {
+
+        public int Id { get; set; }
+        public string Compania { get; set; }
+        public string Contacto { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+    }
+}
